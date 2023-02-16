f31e9e3963a10a6bbdfec373e72ae52384814f8b
roshan <roshan@pm.me>
2023-02-16 18:27:57 +0530

    style: code refactored

--- index.js
+++ index.js
@@ -26,30 +26,35 @@ app.get('/api/', async function (req, res) {
     if(text){
         // intiate browser
         await openBrowser();
-        const bg = req.query.bg ? req.query.bg : 'rgba%28171%2C+184%2C+195%2C+1%29';
-        const t = req.query.t ? req.query.t : 'seti';
-        const wt = req.query.wt ? req.query.wt : 'none';
-        const l = req.query.l ? req.query.l : 'auto';
-        const ds = req.query.ds ? req.query.ds : 'true';
-        const dsyoff = req.query.dsyoff ? req.query.dsyoff : '20px';
-        const blur = req.query.blur ? req.query.blur : '68px';
-        const wc = req.query.wc ? req.query.wc : 'true';
-        const wa = req.query.wa ? req.query.wa : 'true';
-        const pv = req.query.pv ? req.query.pv : '56px';
-        const ph = req.query.ph ? req.query.ph : '56px';
-        const ln = req.query.ln ? req.query.ln : 'false';
-        const fl = req.query.fl ? req.query.fl : '1';
-        const fm = req.query.fm ? req.query.fm : 'Hack';
-        const fs = req.query.fs ? req.query.fs : '14px';
-        const lh = req.query.lh ? req.query.lh : '133%25';
-        const si = req.query.si ? req.query.si : 'false';
-        const es = req.query.es ? req.query.es : '2x';
-        const wm = req.query.wm ? req.query.wm : 'false';
-        const type = req.query.type ? req.query.type : 'png'; 
-        const name = req.query.name ? req.query.name : 'carbon';
-    
-        const options = `bg=${bg}&t=${t}&wt=${wt}&l=${l}&ds=${ds}&dsyoff=${dsyoff}&blur=${blur}&wc=${wc}&wa=${wa}&pv=${pv}&ph=${ph}&ln=${ln}&fl=${fl}&fm=${fm}&fs=${fs}&lh=${lh}&si=${si}&es=${es}&wm=${wm}`
-    
+        
+        const params = {
+            bg      : req.query.bg      || 'rgba%28171%2C+184%2C+195%2C+1%29',
+            t       : req.query.t       || 'seti',
+            wt      : req.query.wt      || 'none',
+            l       : req.query.l       || 'auto',
+            ds      : req.query.ds      || 'true',
+            dsyoff  : req.query.dsyoff  || '20px',
+            blur    : req.query.blur    || '68px',
+            wc      : req.query.wc      || 'true',
+            wa      : req.query.wa      || 'true',
+            pv      : req.query.pv      || '56px',
+            ph      : req.query.ph      || '56px',
+            ln      : req.query.ln      || 'false',
+            fl      : req.query.fl      || '1',
+            fm      : req.query.fm      || 'Hack',
+            fs      : req.query.fs      || '14px',
+            lh      : req.query.lh      || '133%25',
+            si      : req.query.si      || 'false',
+            es      : req.query.es      || '2x',
+            wm      : req.query.wm      || 'false',
+            type    : req.query.type    || 'png',
+            name    : req.query.name    || 'carbon',
+        }
+
+        const options = Object.keys(params)
+            .map(k => k + '=' + params[k])
+            .join('&')
+
         //! Code to get image.
         const page = await browser.newPage();
         await page.setViewport({ width: 1920, height: 1080});
